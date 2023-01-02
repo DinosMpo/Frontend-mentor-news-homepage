4a8b3ae67282743cbd9dd58c822486d339140193
DinosMpo <dinos7591@yahoo.gr>
2023-01-02 14:36:38 +0200

    refactor the code from a class to a function

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Menu from './Menu/Menu';
 import MainSection from './MainSection/MainSection';
 import SideSection from './SideSection/SideSection';
 import BottomSection from './BottomSection/BottomSection';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Menu />
-        
-        <MainSection />
-        
-        <SideSection />
-
-        <BottomSection />
-
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
-        </div>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <MainSection />
+      <SideSection />
+      <BottomSection />
+    </div>
+  )
 }
-
-export default App;
