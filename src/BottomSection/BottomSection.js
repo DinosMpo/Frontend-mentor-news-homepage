b339ae5860ceafbcf2e508dd77e2859ece78b0f6
DinosMpo <dinos7591@yahoo.gr>
2023-01-02 14:41:30 +0200

    creating the bottom section component

--- src/BottomSection/BottomSection.js
+++ src/BottomSection/BottomSection.js
@@ -4,17 +4,50 @@ import './BottomSection.css';
 export default function BottomSection() {
 	return (
 		<div className='bottom-section'>
-          01
-          Reviving Retro PCs
-          What happens when old PCs are given modern upgrades?
+            <div className='bottom-news'>
+                <div className='bottom-news-image'><img src='assets/images/image-retro-pcs.jpg' /></div>
+                <div className='bottom-news-wrapper'>
+                    <div className='bottom-news-number'>
+                        01
+                    </div>
+                    <div className='bottom-news-title'>
+                        Reviving Retro PCs
+                    </div>
+                    <div className='bottom-news-description'>
+                        What happens when old PCs are given modern upgrades?
+                    </div>
+                </div>
+            </div>
+            
+            <div className='bottom-news'>
+                <div className='bottom-news-image'><img src='assets/images/image-top-laptops.jpg' /></div>
+                <div className='bottom-news-wrapper'>
+                    <div className='bottom-news-number'>
+                        02
+                    </div>
+                    <div className='bottom-news-title'>
+                        Top 10 Laptops of 2022
+                    </div>
+                    <div className='bottom-news-description'>
+                        Our best picks for various needs and budgets.
+                    </div>
+                </div>
+            </div>
 
-          02
-          Top 10 Laptops of 2022
-          Our best picks for various needs and budgets.
-
-          03
-          The Growth of Gaming
-          How the pandemic has sparked fresh opportunities.
+            <div className='bottom-news'>
+                <div className='bottom-news-image'><img src='assets/images/image-gaming-growth.jpg' /></div>
+                <div className='bottom-news-wrapper'>
+                    <div className='bottom-news-number'>
+                        03
+                    </div>
+                    <div className='bottom-news-title'>
+                        The Growth of Gaming
+                    </div>
+                    <div className='bottom-news-description'>
+                        How the pandemic has sparked fresh opportunities.
+                    </div>
+                </div>
+            </div>
         </div>
 	)
 }
